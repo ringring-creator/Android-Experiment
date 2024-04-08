61105ac389d86153615aeb01323f8f62efe9babe
ringring <ringring.creator@gmail.com>
2024-04-08 13:16:49 +0900

    Set theme for material3

--- android/src/androidMain/kotlin/App.kt
+++ android/src/androidMain/kotlin/App.kt
@@ -1,12 +1,10 @@
-import androidx.compose.material.MaterialTheme
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.tooling.preview.Preview
 import com.ring.ring.login.LoginScreen
+import com.ring.ring.theme.AndroidExperimentTheme
 
 @Composable
-@Preview
 fun App() {
-    MaterialTheme {
+    AndroidExperimentTheme {
         LoginScreen({}, {})
     }
 }
